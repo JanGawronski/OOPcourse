89830f23a3defb3642b9f27e771ec5e0c8a95c34
JanGawronski <jangawronski04@gmail.com>
2024-12-11 00:14:44 +0100

    Added advanced GUI

--- app/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ app/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -5,14 +5,18 @@ import java.util.List;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 import agh.ics.oop.Simulation;
 import agh.ics.oop.SimulationApp;
-import agh.ics.oop.SimulationEngine;
 import agh.ics.oop.model.AbstractWorldMap;
+import agh.ics.oop.model.Boundary;
 import agh.ics.oop.model.GrassField;
 import agh.ics.oop.model.MapChangeListener;
 import agh.ics.oop.model.MoveDirection;
@@ -23,14 +27,59 @@ import agh.ics.oop.OptionsParser;
 public class SimulationPresenter implements MapChangeListener {
     private WorldMap map;
     @FXML
-    private Label mapGrid;
+    private GridPane mapGrid;
     @FXML
     private TextField moveListTextField;
     @FXML
     private Label moveDescriptionLabel;
 
-    public void drawMap(WorldMap map) {
-        mapGrid.setText(map.toString());
+    private final int CELL_WIDTH = 50;
+    private final int CELL_HEIGHT = 50;
+
+    private void drawMap(WorldMap map) {
+        clearGrid();
+
+        Boundary bounds = map.getCurrentBounds();
+
+        int width = bounds.upperRight().getX() - bounds.lowerLeft().getX() + 1;
+        int height = bounds.upperRight().getY() - bounds.lowerLeft().getY() + 1;
+
+        for (int i = 0; i < width; i++) {
+            mapGrid.getColumnConstraints().add(new ColumnConstraints(CELL_WIDTH));
+            Label label = new Label(Integer.toString(i + bounds.lowerLeft().getX()));
+            mapGrid.add(label, i + 1, 0);
+            GridPane.setHalignment(label, HPos.CENTER);
+        }
+
+        for (int i = 0; i < height; i++) {
+            mapGrid.getRowConstraints().add(new RowConstraints(CELL_HEIGHT));
+            Label label = new Label(Integer.toString(bounds.upperRight().getY() - i));
+            mapGrid.add(label, 0, i + 1);
+            GridPane.setHalignment(label, HPos.CENTER);
+        }
+
+        for (int i = 0; i < width; i++) {
+            for (int j = height - 1; j >= 0; j--) {
+                Vector2d position = new Vector2d(i + bounds.lowerLeft().getX(), bounds.upperRight().getY() - j);
+                if (map.isOccupied(position)) {
+                    Label label = new Label(map.objectAt(position).toString());
+                    mapGrid.add(label, i + 1, j + 1);
+                    GridPane.setHalignment(label, HPos.CENTER);
+                }
+            }
+        }
+
+        mapGrid.getColumnConstraints().add(new ColumnConstraints(CELL_HEIGHT));
+        mapGrid.getRowConstraints().add(new RowConstraints(CELL_WIDTH));
+        Label label = new Label("y\\x");
+        mapGrid.add(label, 0, 0);
+        GridPane.setHalignment(label, HPos.CENTER);
+    }
+
+    private void clearGrid() {
+        mapGrid.getChildren().retainAll(mapGrid.getChildren().get(0));
+        mapGrid.getColumnConstraints().clear();
+        mapGrid.getRowConstraints().clear();
     }
 
     public void setMap(WorldMap map) {
@@ -48,21 +97,19 @@ public class SimulationPresenter implements MapChangeListener {
     @FXML
     public void onSimulationStartClicked() {
         String moveList = moveListTextField.getText();
-        List<MoveDirection> directions = OptionsParser.parse(moveList.split(" "));
-        List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,4));
+        List<MoveDirection> directions = OptionsParser.parse(moveList.split("\\s+"));
+        List<Vector2d> positions = List.of(new Vector2d(2, 2), new Vector2d(3, 4));
         AbstractWorldMap map = new GrassField(10);
         setMap(map);
         map.addObserver(this);
         Simulation simulation = new Simulation(positions, directions, map);
-        SimulationEngine engine = new SimulationEngine(List.of(simulation));
         moveDescriptionLabel.setText("Simulation started with moves: " + moveList);
-        new Thread(() -> {
-            engine.runSync();
-        }).start();
+        Thread thread = new Thread(simulation);
+        thread.start();
     }
 
     @FXML
-    private void onNewSimulationClicked(){
+    private void onNewSimulationClicked() {
         SimulationApp simulationApp = new SimulationApp();
         try {
             simulationApp.start(new Stage());
@@ -70,7 +117,4 @@ public class SimulationPresenter implements MapChangeListener {
             e.printStackTrace();
         }
     }
-
-
-
 }
